0cb162a9aaff9c8d8224d7194a927c167b2fae78
刘屹 <liuyi08@wondersgroup.com>
2021-04-27 22:29:48 +0800

    modelAndView

--- spring-springMVC/src/main/java/controller/UserController.java
+++ spring-springMVC/src/main/java/controller/UserController.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -11,5 +12,20 @@ public class UserController {
         System.out.println("Controller save running...");
         //加"/"是为了访问外部success的文件
         return "success";
+    }
+
+    @RequestMapping("/quick2")
+    public ModelAndView save2(){
+        /*
+            Model:模型 作用：封装数据
+            View:视图  作用：展示数据
+         */
+        ModelAndView modelAndView = new ModelAndView();
+        // 设置模型数据
+        modelAndView.addObject("username","it");
+        // 设置视图名称
+        modelAndView.setViewName("success");
+
+        return modelAndView;
     }
 }
